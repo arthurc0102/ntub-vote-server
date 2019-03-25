919a657f235166495b827f32c826a61e6f16c62c
Arthur Chang <arthurc0102@gmail.com>
2019-03-25 18:37:32 +0800

    feat: Setup static and logging

---
+++ config/components/logging.py
@@ -0,0 +1,25 @@
+from config.settings import env
+
+
+# Logging SQL
+
+LOG_SQL = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
+
+if env('LOG_SQL', default=False):
+    LOGGING = LOG_SQL
