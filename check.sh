f3d9d2bd9d62790f22d4fbc79a60b39068e874bd
Arthur Chang <arthurc0102@gmail.com>
2019-05-07 12:43:49 +0800

    feat: Add checking migrations and rename file

--- check.sh
+++ check.sh
@@ -12,3 +12,14 @@ flake8 --exclude=venv,env,.venv,migrations
 if [ $? -eq 0 ]; then
   echo 'Success'
 fi
+
+
+echo ''
+echo 'Checking migrations'
+python manage.py makemigrations --check --dry-run > /dev/null
+
+if [ $? -eq 0 ]; then
+  echo 'Success'
+else
+  echo 'Missing migrations'
+fi
